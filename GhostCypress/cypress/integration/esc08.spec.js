940ba4869fe3096faeb02be11ecbe289090619dd
Luis Correa <l.correaz@uniandes.edu.co>
2022-05-11 19:58:06 -0500

    se agregan SC[04, 08, 12, 16, 20] en cypress

---
+++ GhostCypress/cypress/integration/esc08.spec.js
@@ -0,0 +1,38 @@
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // we expect a 3rd party library error with message 'list not defined'
+    // and don't want to fail the test so we return false
+    if (err.message.includes('n.includes is not a function')) {
+      return false
+    }
+    // we still want to ensure there are no other unexpected
+    // errors, so we let them fail the test
+  })
+
+const url = Cypress.config('baseUrl') 
+const username = Cypress.env('username')
+const password = Cypress.env('password')
+
+describe('Visit site login', () => {
+    beforeEach(()=>{
+       cy.visit("/")
+        cy.wait(4000)
+    })
+       it('Login to ghost, change password and logout', () => {  
+        cy.get('form').within(() => {
+            cy.get('input[id="ember8"]').type(username)
+            cy.get('input[id="ember10"]').type(password)
+            cy.get('.login.gh-btn').click()
+        })
+        cy.get('.ember-view.gh-secondary-action.gh-nav-new-post').click();
+        cy.wait(1000);
+        cy.get('.gh-editor-title.ember-text-area.gh-input.ember-view').type('POST DE PRUEBA PARA BORRAR');
+        cy.wait(2000);
+        cy.get('.koenig-editor__editor').type("PRUEBAS AUTOMATIZADAS MISO");
+        cy.wait(3000);
+        cy.get('.ember-view.items-center').click();
+        cy.wait(2000);
+
+    })
+
+})
