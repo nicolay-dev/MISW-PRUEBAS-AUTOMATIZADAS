3c1f12e8045aa3ab601fd54bd579d9a3c78857e3
Luis Correa <l.correaz@uniandes.edu.co>
2022-05-22 16:07:07 -0500

    resolve union on pseudodinamic sc

---
+++ PruebasAleatorias/GhostCypress SeudoDinamico/cypress/integration/esc12.spec.js
@@ -0,0 +1,114 @@
+import {faker} from '@faker-js/faker'
+
+const POM = require("../POM/POM")
+const url = Cypress.config('baseUrl')
+const pollData01 = Cypress.env('poolData01');
+
+const username = pollData01.username;
+const password = pollData01.password;
+const titulo = faker.lorem.word(12);
+const parrafo = faker.lorem.paragraph(2);
+const excerpt = faker.random.alphaNumeric(299);
+const excerpt03 = faker.random.alphaNumeric(300);
+const excerpt04 = faker.random.alphaNumeric(301);
+
+
+describe('Create excerpt description in post', () => {
+    beforeEach(()=>{
+       cy.visit("/")
+        cy.wait(4000)
+        
+    })
+       it('Login to ghost, create post, add title, add content and set excerpt', () => {  
+        cy.get('form').within(() => {
+          POM.signIn(username, password);
+          
+        })
+        cy.wait(1000)
+        //Build a new post
+        POM.buildNewPost(titulo, parrafo)
+        cy.wait(2000)
+        
+        POM.clickSettingsOnPP()
+        cy.wait(2000);
+        
+        //Create Excertp
+        cy.get('.ember-text-area.post-setting-custom-excerpt').type(excerpt);
+        cy.wait(2000);
+        
+        POM.closeSettings()
+        cy.wait(2000);
+        
+        //Pusblish Post
+        POM.publishUpdatePP()
+        cy.wait(2000);
+        
+
+    })
+    it('Login to ghost, create post, add title, add content and set excerpt3', () => {  
+      cy.get('form').within(() => {
+        POM.signIn(username, password);
+        
+      })
+      cy.wait(1000)
+      //Build a new post
+      POM.buildNewPost(titulo, parrafo)
+      cy.wait(2000)
+      
+      POM.clickSettingsOnPP()
+      cy.wait(2000);
+      
+      //Create Excertp
+      cy.get('.ember-text-area.post-setting-custom-excerpt').type(excerpt03);
+      cy.wait(2000);
+      
+      POM.closeSettings()
+      cy.wait(2000);
+      
+      //Pusblish Post
+      POM.publishUpdatePP()
+      cy.wait(2000);
+      
+
+  })
+
+  it('Login to ghost, create post, add title, add content and set excerpt 4 ', () => {  
+    cy.get('form').within(() => {
+      POM.signIn(username, password);
+      
+    })
+    cy.wait(1000)
+    //Build a new post
+    POM.buildNewPost(titulo, parrafo)
+    cy.wait(2000)
+    
+    POM.clickSettingsOnPP()
+    cy.wait(2000);
+    
+    //Create Excertp
+    cy.get('.ember-text-area.post-setting-custom-excerpt').type(excerpt04);
+    
+    //Click on empty space
+    cy.get('.settings-menu-header').click();
+    cy.wait(2000);
+    cy.get('button[title="Settings"]');
+    cy.wait(2000);
+    cy.get('div[class="form-group error ember-view"] p[class="response"]').contains('Excerpt cannot be longer than 300 characters.')
+    cy.wait(2000);
+    cy.get('#custom-excerpt').clear().type(excerpt);
+    cy.wait(2000);
+
+    
+
+    POM.closeSettings()
+    cy.wait(2000);
+    
+    //Pusblish Post
+    POM.publishUpdatePP()
+    cy.wait(2000);
+    
+  
+})
+
+
+})
--- PruebasAleatorias/GhostCypress SeudoDinamico/cypress/integration/esc16.spec.js
+++ PruebasAleatorias/GhostCypress SeudoDinamico/cypress/integration/esc16.spec.js
@@ -1,4 +1,5 @@
 import {faker} from '@faker-js/faker'
+
 const POM = require("../POM/POM")
 const url = Cypress.config('baseUrl')
 const pollData01 = Cypress.env('poolData01');
@@ -8,149 +9,149 @@ const password = pollData01.password;
 const titulo = faker.lorem.word(10);
 const parrafo = faker.lorem.paragraph(2)
 const tituloT = faker.random.alphaNumeric(300);
-const titulo5F = faker.random.alphaNumeric(301);
-const titulo5G = faker.random.words(2);
-const description7F = faker.random.alphaNumeric(500);
+const tituloT5 = faker.random.alphaNumeric(301);
+const titulogG5 = faker.random.words(2);
+const description7T = faker.random.alphaNumeric(500);
 const description8T = faker.random.alphaNumeric(501);
 const description8C = faker.random.alphaNumeric(300);
 
-describe('Create post with Facebook card', () => {
+let count = 0; 
+
+describe('Create a Twitter title card', () => {
     beforeEach(()=>{
        cy.visit("/")
         cy.wait(4000)
-
+        
     })
-       it('Login to ghost, add title, add content and create facebook card', () => {  
+       it('Login to ghost, add title, add content, add twitter card with description.', () => {  
         cy.get('form').within(() => {
           POM.signIn(username, password);
-  
         })
         cy.wait(1000);
-        //Create new post
+        
+        //Create post
         POM.buildNewPost(titulo,parrafo)
         cy.wait(2000)
-
+        
         POM.clickSettingsOnPP()
         cy.wait(2000)
-
-        //Add facebook card
-        cy.get('b').contains("Facebook card").click();       
+        
+        //Create Twitter CArd
+        cy.get('b').contains("Twitter card").click();       
         cy.wait(1000);
 
-        //Sert Card title
-        cy.get('#og-title').type(tituloT);
+        //Add card title
+        cy.get('#twitter-title').type(tituloT);
         cy.wait(1000);
         
-
-        cy.get('.ember-view.gh-app ').click();
+        cy.get('.ember-view.gh-app ').click();        
         cy.wait(2000);
-
+        
         //Publish post
         POM.publishUpdatePP()
         cy.wait(2000);
-
+        
     })
-    it('Login to ghost, add title, add content and create facebook card5', () => {  
+    it('Login to ghost, add title, add content, add twitter card with description.5', () => {  
       cy.get('form').within(() => {
         POM.signIn(username, password);
-
       })
       cy.wait(1000);
-      //Create new post
+      
+      //Create post
       POM.buildNewPost(titulo,parrafo)
       cy.wait(2000)
-
+      
       POM.clickSettingsOnPP()
       cy.wait(2000)
-
-      //Add facebook card
-      cy.get('b').contains("Facebook card").click();       
+      
+      //Create Twitter CArd
+      cy.get('b').contains("Twitter card").click();       
       cy.wait(1000);
 
-      //Sert Card title
-      cy.get('#og-title').type(titulo5F);
+      //Add card title
+      cy.get('#twitter-title').type(tituloT5);
       cy.wait(1000);
       cy.get('.active > .settings-menu-header').click()
       cy.wait(1000);
-      cy.get('div[class="form-group error ember-view"] p[class="response"]').contains('Facebook Title cannot be longer than 300 characters. ')
+      cy.get('div[class="form-group error ember-view"] p[class="response"]').contains('Twitter Title cannot be longer than 300 characters. ')
       cy.wait(1000);
-      cy.get('#og-title').clear().type(titulo5G); 
-      
+      cy.get('#twitter-title').clear().type(titulogG5);            
 
-      cy.get('.ember-view.gh-app ').click();
+      cy.get('.ember-view.gh-app ').click();        
       cy.wait(2000);
-
+      
       //Publish post
       POM.publishUpdatePP()
       cy.wait(2000);
-
+      
   })
-
-  it('Login to ghost, add title, add content and create facebook card7', () => {  
+  it('Login to ghost, add title, add content, add twitter card with description7.', () => {  
     cy.get('form').within(() => {
       POM.signIn(username, password);
-
     })
     cy.wait(1000);
-    //Create new post
+    
+    //Create post
     POM.buildNewPost(titulo,parrafo)
     cy.wait(2000)
-
+    
     POM.clickSettingsOnPP()
     cy.wait(2000)
-
-    //Add facebook card
-    cy.get('b').contains("Facebook card").click();       
+    
+    //Create Twitter CArd
+    cy.get('b').contains("Twitter card").click();       
     cy.wait(1000);
 
-    //Set Card title
-    cy.get('#og-title').type(titulo);
-    cy.get('#og-description').type(description7F);
+    //Add card title
+           
+    cy.get('#twitter-title').type(titulo);
+    cy.get('#twitter-description').type(description7T);
     cy.wait(1000);
     cy.get('.ember-view.gh-app ').click();        
     cy.wait(2000);
-
+    
     //Publish post
     POM.publishUpdatePP()
     cy.wait(2000);
-
+    
 })
 
-it('Login to ghost, add title, add content and create facebook card 8', () => {  
+it('Login to ghost, add title, add content, add twitter card with description 8.', () => {  
   cy.get('form').within(() => {
     POM.signIn(username, password);
-
   })
   cy.wait(1000);
-  //Create new post
+  
+  //Create post
   POM.buildNewPost(titulo,parrafo)
   cy.wait(2000)
-
+  
   POM.clickSettingsOnPP()
   cy.wait(2000)
-
-  //Add facebook card
-  cy.get('b').contains("Facebook card").click();       
+  
+  //Create Twitter CArd
+  cy.get('b').contains("Twitter card").click();       
   cy.wait(1000);
 
-  //Set Card title
-  cy.get('#og-title').type(titulo);
-  cy.get('#og-description').type(description8T);
+  //Add card title
+         
+  cy.get('#twitter-title').type(titulo);
+  cy.get('#twitter-description').type(description8T);
   cy.wait(1000);
   cy.get('.active > .settings-menu-header').click()
   cy.wait(1000);
-  cy.get('div[class="form-group error ember-view"] p[class="response"]').contains('Facebook Description cannot be longer than 500 characters. ')
+  cy.get('div[class="form-group error ember-view"] p[class="response"]').contains('Twitter Description cannot be longer than 500 characters. ')
   cy.wait(1000);
-  cy.get('#og-description').clear().type(description8C);  
+  cy.get('#twitter-description').clear().type(description8C);            
 
-  cy.wait(1000);
   cy.get('.ember-view.gh-app ').click();        
   cy.wait(2000);
-
+  
   //Publish post
   POM.publishUpdatePP()
   cy.wait(2000);
-
+  
 })
 
 })
