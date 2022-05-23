202b974ae3088f8cbfca2768d16d1e3fb5476c3d
Nicolay J <nicolay.dev@outlook.com>
2022-05-22 20:03:31 -0500

    cas3 11

--- PruebasAleatorias/GhostCypress Apriori/cypress/integration/esc07.spec.js
+++ PruebasAleatorias/GhostCypress Apriori/cypress/integration/esc07.spec.js
@@ -17,33 +17,33 @@ describe('Schedule a post', () => {
   beforeEach(() => {
     cy.visit("/")
     cy.wait(4000)
-      takeScreenshots ? POM.takeScreenShot('esc03', screenShotCount++): {};
+      takeScreenshots ? POM.takeScreenShot('esc07', screenShotCount++): {};
   })
   it('Create post and delete it', () => {
     cy.get('form').within(() => {
       POM.signIn(username, password);
-        takeScreenshots ? POM.takeScreenShot('esc03', screenShotCount++): {};
+        takeScreenshots ? POM.takeScreenShot('esc07', screenShotCount++): {};
     })
     cy.wait(1000);
     //Build a new post
     POM.buildNewPost(titulo, parrafo);
     cy.wait(1000);
-    takeScreenshots ? POM.takeScreenShot('esc03', screenShotCount++): {};
+    takeScreenshots ? POM.takeScreenShot('esc07', screenShotCount++): {};
     cy.get('span').contains('Publish').click();
     cy.wait(1000);
-    takeScreenshots ? POM.takeScreenShot('esc03', screenShotCount++): {};
+    takeScreenshots ? POM.takeScreenShot('esc07', screenShotCount++): {};
     cy.get('div').contains('Schedule it for later').click();
     cy.wait(1000);
-    takeScreenshots ? POM.takeScreenShot('esc03', screenShotCount++): {};
+    takeScreenshots ? POM.takeScreenShot('esc07', screenShotCount++): {};
     cy.get('span').contains('Schedule').click();
     cy.wait(301000);
     POM.goToPosts();
-    takeScreenshots ? POM.takeScreenShot('esc03', screenShotCount++): {};
+    takeScreenshots ? POM.takeScreenShot('esc07', screenShotCount++): {};
     // check if Post7 was create
     cy.get('a[title="Edit this post"]').then(($tag) => {
       expect($tag[1].innerText).to.includes(titulo);
     }) 
-    takeScreenshots ? POM.takeScreenShot('esc03', screenShotCount++): {};
+    takeScreenshots ? POM.takeScreenShot('esc07', screenShotCount++): {};
 
   })
 
--- PruebasAleatorias/GhostCypress Apriori/cypress/integration/esc11.spec.js
+++ PruebasAleatorias/GhostCypress Apriori/cypress/integration/esc11.spec.js
@@ -3,9 +3,11 @@
 const POM = require("../POM/POM")
 const url = Cypress.config('baseUrl')
 const pollData01 = Cypress.env('poolData01');
+const takeScreenshots = false;
 
-const username = pollData01.username;
-const password = pollData01.password;
+
+const username = pollData01.genericUsername;
+const password = pollData01.genericPassword;
 const bio = pollData01.BIOTEXT;
 let count=0;
 
@@ -13,34 +15,35 @@ describe('Add Bio', () => {
   beforeEach(() => {
     cy.visit("/")
     cy.wait(4000)
-    POM.takeScreenShot('esc11', count++);
+    takeScreenshots ? POM.takeScreenShot('esc11', screenShotCount++): {};
   })
-  it('Create post and delete it', () => {
+  it('Agregar bio', () => {
     cy.get('form').within(() => {
       POM.signIn(username, password);
-      POM.takeScreenShot('esc11', count++);
+      takeScreenshots ? POM.takeScreenShot('esc11', screenShotCount++): {};
     })
     cy.wait(1000);
     //Go to the staff owner page
     POM.elements.emailInput().should('not.exist'); 
     POM.goToStaff();
     cy.wait(1000);
-    POM.takeScreenShot('esc11', count++);
+    takeScreenshots ? POM.takeScreenShot('esc11', screenShotCount++): {};
     cy.get('.gh-badge.owner').click();
     cy.wait(1000);
-    POM.takeScreenShot('esc11', count++);
+    takeScreenshots ? POM.takeScreenShot('esc11', screenShotCount++): {};
     cy.get(`textarea[id="user-bio"]`).type(bio);
     cy.wait(1000);
-    POM.takeScreenShot('esc11', count++);
+    takeScreenshots ? POM.takeScreenShot('esc11', screenShotCount++): {};
     cy.get('span').contains('Save').click();
     cy.wait(1000);
-    POM.takeScreenShot('esc11', count++);
+    takeScreenshots ? POM.takeScreenShot('esc11', screenShotCount++): {};
     POM.elements.emailInput().should('not.exist'); 
     POM.goToStaff();
     cy.wait(1000);
-    POM.takeScreenShot('esc11', count++);
+    takeScreenshots ? POM.takeScreenShot('esc11', screenShotCount++): {};
     cy.get('.gh-badge.owner').click();
-    cy.get('textarea[id="user-bio"]');
-    POM.takeScreenShot('esc11', count++);
+    cy.wait(1000);
+    cy.get(`textarea[id="user-bio"]`).should('exist')
+    takeScreenshots ? POM.takeScreenShot('esc11', screenShotCount++): {};
   })
 })
